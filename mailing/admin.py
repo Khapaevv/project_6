0e20d32a859c5b653449e1cdf2cfb7b744e5c50d
Vladimir Khapaev <khapaev@mail.ru>
2024-09-24 18:23:57 +0300

    mailing/admin is done

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
-from mailing.models import Client
+from mailing.models import Client, Message, Mailing, MailingLog
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ("id", "email")
+    list_display = ("id", "name", "email")
     list_filter = ("email",)
-    verbose_name = "Клиенты"
+    search_fields = ("name",)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "message_theme", "message_body")
+    list_filter = ("message_theme",)
+    search_fields = ("message_theme",)
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_date", "next_date", "last_date", "clients", "intervals", "mailing_status")
+    list_filter = ("clients", "intervals", "mailing_status")
+    search_fields = ("clients", "intervals", "mailing_status")
+
+
+@admin.register(MailingLog)
+class MailingLogAdmin(admin.ModelAdmin):
+    list_display = ("id", "last_mailing", "status_mailing", "mail_response", "mailing")
+    list_filter = ("last_mailing", "status_mailing")
+    search_fields = ("status_mailing")
+
+
